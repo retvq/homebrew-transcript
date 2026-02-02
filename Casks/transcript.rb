c6fa76516686c8b062df67fc646a5e3413726f05
retvq <ritvik2403@gmail.com>
2026-02-02 10:15:11 +0530

    Added Transcript Homebrew Cask.

---
+++ Casks/transcript.rb
@@ -0,0 +1,11 @@
+cask "transcript" do
+  version "1.0.0"
+  sha256 "6B83BC4B69125376AC2801416A131450CF52C428706C50B874DBB5DF3A3E3DE4"
+
+  url "https://github.com/retvq/transcript/releases/download/v1.0.0/Transcript_1.0.0_aarch64_apple_darwin.dmg"
+  name "Transcript"
+  desc "Offline speech-to-text tool"
+  homepage "https://github.com/retvq/transcript"
+
+  app "Transcript.app"
+end
